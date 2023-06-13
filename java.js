5f810575d1cabd7ecc1f37e8c670f29bdab3f87b
aramn0101 <129150860+aramn0101@users.noreply.github.com>
2023-06-13 11:51:38 +0530

    juj
    j

--- java.js
+++ java.js
@@ -1,178 +1,23 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBjoOgVcuBxqIJLWl_Rgp9y8woxIGf8jjQ",
-  authDomain: "phoneotp-1ae30.firebaseapp.com",
-  databaseURL: "https://phoneotp-1ae30-default-rtdb.firebaseio.com",
-  projectId: "phoneotp-1ae30",
-  storageBucket: "phoneotp-1ae30.appspot.com",
-  messagingSenderId: "260001980169",
-  appId: "1:260001980169:web:b84a13bb833e7523a93dd8",
-  measurementId: "G-J32LV9Z4VZ",
-};
+function sign() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
 
-firebase.initializeApp(firebaseConfig);
-
-render();
-function render() {
-  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-    "recaptcha-container"
-  );
-  recaptchaVerifier.render();
-}
-/*
-function phoneAuth() {
-  var number = "+91" + document.getElementById("number").value;
-
-  firebase
-    .auth()
-    .signInWithPhoneNumber(number, window.recaptchaVerifier)
-    .then(function (confirmationResult) {
-      window.confirmationResult = confirmationResult;
-      coderesult = confirmationResult;
-      document.getElementById("sender").style.display = "none";
-      document.getElementById("verifier").style.display = "block";
-    })
-    .catch(function (error) {
-      alert(error.message);
-    });
+  localStorage.setItem("name1", name);
+  localStorage.setItem("eami1", email);
+  localStorage.setItem("password1", password);
+  localStorage.setItem("login", true);
 }
 
-function codeverify() {
-  var code = document.getElementById("verificationcode").value;
-  coderesult
-    .confirm(code)
-    .then(function () {
-      document.getElementById("p-conf")[0].style.display = "block";
-      document.getElementById("n-conf")[0].style.display = "none";
-    })
-    .catch(function () {
-      document.getElementById("p-conf")[0].style.display = "none";
-      document.getElementById("n-conf")[0].style.display = "block";
-    });
-}
-*/
+function login() {
+  var name1 = document.getElementById("name1").value;
+  var password1 = document.getElementById("password1").value;
 
-function phoneAuth() {
-  var number = "+91" + document.getElementById("number").value;
-  var number1 = "+91" + document.getElementById("number").value;
-  var ref = firebase.database().ref().push();
-  var key = ref.key;
-  firebase
-    .auth()
-    .signInWithPhoneNumber(number, window.recaptchaVerifier)
-    .then(function (confirmationResult) {
-      window.confirmationResult = confirmationResult;
-      coderesult = confirmationResult;
-      console.log(coderesult);
-      firebase
-        .database()
-        .ref("Number/" + key)
-        .set({
-          number: number1,
-        });
-      alert("meassge sent");
-    })
-    .catch(function (error) {
-      alert(error.message);
-    });
-}
-function codeverify() {
-  var code = document.getElementById("verifiycode").value;
-  coderesult
-    .confirm(code)
-    .then(function (result) {
-      var user = result.user;
-      console.log(user);
-      alert("Successfully registered");
-    })
-    .catch(function (error) {
-      if (error) {
-        alert("invaild otp");
-      }
-    });
-}
-
-/*
-render();
-function render() {
-  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-    "recaptcha-container"
-  );
-  recaptchaVerifier.render();
-}
-*/
-/*
-function login1() {
-  var st = (document.getElementById("form").style.display = "none");
-  if (st) {
-    document.getElementById("form1").style.display = "none";
-    document.getElementById("form2").style.display = "block";
-    document.getElementById("you").style.display = "none";
-    document.getElementById("form").style.display = "none";
-    document.getElementById("recaptcha-container").style.display = "block";
+  var name2 = localStorage.getItem("name1");
+  var password2 = localStorage.getItem("password1");
+  if (name1 == name2 && password1 == password2) {
+    window.location.href = "product.html";
   } else {
-    document.getElementById("form2").style.display = "none";
-    document.getElementById("form2").style.display = "none";
-    document.getElementById("form1").style.display = "none";
-    document.getElementById("recaptcha-container").style.display = "none";
+    alert("incorrect");
   }
 }
-
-render();
-function render() {
-  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-    "recaptcha-containern"
-  );
-  recaptchaVerifier.render();
-}
-function number() {
-  var st = (document.getElementById("form2").style.display = "none");
-  if (st) {
-    document.getElementById("form2").style.display = "none";
-    document.getElementById("form3").style.display = "block";
-  } else {
-    document.getElementById("form2").style.display = "none";
-    document.getElementById("form3").style.display = "none";
-  }
-  var number = "+91" + document.getElementById("numberotp").value;
-  var number1 = "+91" + document.getElementById("numberotp").value;
-  var user = document.getElementById("username").value;
-  var ref = firebase.database().ref().push();
-  var key = ref.key;
-
-  firebase
-    .auth()
-    .signInWithPhoneNumber(number, window.recaptchaVerifier)
-    .then(function (confirmationResult) {
-      window.confirmationResult = confirmationResult;
-      coderesult = confirmationResult;
-      console.log(coderesult);
-      firebase
-        .database()
-        .ref("Number/" + key)
-        .set({
-          user: user,
-          number: number1,
-        });
-      alert("meassge sent");
-    })
-    .catch(function (error) {
-      alert(error.message);
-    });
-}
-
-function ganrate() {
-  var code = document.getElementById("otp").value;
-  coderesult
-    .confirm(code)
-    .then(function (result) {
-      var user = result.user;
-      console.log(user);
-      alert("Successfully registered");
-    })
-    .catch(function (error) {
-      if (error) {
-        alert("invaild otp");
-      }
-    });
-}
-*/
